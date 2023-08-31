1f79d1c4448cb2f75b9eb87ab39a7418895dee4b
Aubrey Zulu <aubreydarious@gmail.com>
2023-09-01 00:16:35 +0200

    test setup and mocks for prisma

---
+++ src/script.ts
@@ -0,0 +1,9 @@
+// script.ts
+import { Prisma } from '@prisma/client';
+import prisma from '../libs/prisma';
+
+export const createUser = async (user: Prisma.UserCreateInput) => {
+  return await prisma.user.create({
+    data: user,
+  });
+};
---
+++ test/sample.test.ts
@@ -0,0 +1,13 @@
+//test/sample.test.ts
+import { expect, test, vi } from 'vitest';
+import { createUser } from '../src/script';
+import prisma from '../libs/__mocks__/prisma';
+
+vi.mock('../libs/prisma');
+
+test('createUser should return the generated user', async () => {
+  const newUser = { email: 'user@prisma.io', name: 'Prisma Fan' };
+  prisma.user.create.mockResolvedValue({ ...newUser, id: 1 });
+  const user = await createUser(newUser);
+  expect(user).toStrictEqual({ ...newUser, id: 1 });
+});
